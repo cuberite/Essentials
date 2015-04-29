4763d64f080ebda234a7367a0979994ac6040f1b
Mathias <mathias-gh@users.noreply.github.com>
2015-04-29 17:26:04 +0300

    Implemented enchantsigns

--- hooks.lua
+++ hooks.lua
@@ -9,12 +9,36 @@ end
 function OnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
 	World = Player:GetWorld()
 	--Check for a sign
-    if (BlockType == E_BLOCK_SIGN) then	
+	if (BlockType == E_BLOCK_SIGN) then	
 		Read, Line1, Line2, Line3, Line4 = World:GetSignLines( BlockX, BlockY, BlockZ)
 		--If the sign is written like it should, teleport the player
 		if Line1 == "[SignWarp]" or Line1 == "[Warp]" then
 			cPluginManager:Get():ExecuteCommand(Player, "/warp "..Line2)
 			return true
+		end
+		--If the sign is written like it should, enchant the item
+		if Line1 == "[Enchant]" and Line2 ~= "" and Line3 ~= "" and Line4 ~= "" then
+			HeldItem = Player:GetEquippedItem();
+			HeldItemType = HeldItem.m_ItemType;
+			ItemEnchant = HeldItem.m_Enchantments;
+			level = Player:GetXpLevel();
+			Enchantment = cEnchantments:StringToEnchantmentID(Line2);
+			MaxLevel = Line3;
+			LevelNeeded = Line4;
+			CurrentItemLevel = HeldItem.m_Enchantments:GetLevel(Enchantment);
+			NextLevel = CurrentItemLevel + 1;
+			toremove = tonumber(Line4) * NextLevel
+			if CurrentItemLevel == tonumber(Line3) or level < tonumber(Line4) then
+				return false
+			else
+				if IsEnchantable() == true then
+					ItemEnchant:SetLevel(Enchantment, NextLevel)
+					Player:GetInventory():SetHotbarSlot(Player:GetInventory():GetEquippedSlotNum(), HeldItem)
+					Player:DeltaExperience(-toremove * 17)
+				else
+					Player:SendMessage("This item is not enchantable")
+				end
+			end
 		end
 	end
 end
@@ -33,6 +57,10 @@ function OnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line
 			return true
 		elseif (Line2 == "") then
 			Player:SendMessageFailure('Must supply a warp to teleport.')
+			return true
+		end
+	elseif Line1 == "[Enchant]" then
+		if (not(Player:HasPermission("es.enchantsign") == true)) then
 			return true
 		end
 	end
