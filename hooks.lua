ba82c27fd4e3fca13015a43e777d2b638b3edfef
Mathias <mathias-github@users.noreply.github.com>
2015-05-14 15:35:49 +0300

    Added success message to EnchantSigns

--- hooks.lua
+++ hooks.lua
@@ -34,8 +34,9 @@ function OnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, 
 					ItemEnchant:SetLevel(Enchantment, NextLevel)
 					Player:GetInventory():SetHotbarSlot(Player:GetInventory():GetEquippedSlotNum(), HeldItem)
 					Player:DeltaExperience(-toremove * 17)
+					Player:SendMessageSuccess("Successfully enchanted item")
 				else
-					Player:SendMessage("This item is not enchantable")
+					Player:SendMessageWarning("This item is not enchantable")
 				end
 			end
 		end
