e985e32d69b565fdafa4ad6c335f9b95527b4963
Mathias <mathias-github@users.noreply.github.com>
2015-07-24 18:42:50 +0300

    Changed core permission to es

--- cheat.lua
+++ cheat.lua
@@ -218,60 +218,45 @@ function HandleRunSpeedCommand(Split, Player)
 end
 
 function HandleFlyCommand(Split, Player)
-	
 	local function ChangeFly( newPlayer )
 		newPlayer:SetCanFly(not newPlayer:CanFly());
 		newPlayer:SendMessageSuccess("Fly mode toggled!")
 		return true
 	end
-	
 	if (Split[2] == nil or Split[2] == Player:GetName()) then
 		ChangeFly(Player)
 	elseif (Player:HasPermission("es.fly.other")) then
-		
 		if cRoot:Get():FindAndDoWithPlayer( Split[2], ChangeFly ) then
 			Player:SendMessageSuccess("Fly mode for player " .. Split[2] ..  " toggled!")
 		else
 			Player:SendMessageFailure("Player not found")
 		end
-		
 	else 
 		Player:SendMessageFailure("You need es.fly.other permission to do that!")
 	end
-	
 	return true
-	
 end
 
 function HandleVanishCommand(Split, Player)
-	
 	local function ChangeVanish( newPlayer )
-	
 		newPlayer:SetVisible(not newPlayer:IsVisible());
-	
 		if newPlayer:IsVisible() then
 			newPlayer:SendMessageSuccess("You aren't hidden anymore!")
 		else
 			newPlayer:SendMessageSuccess("You are now hidden!")
 		end
-		
 		return true
 	end
-	
 	if (Split[2] == nil or Split[2] == Player:GetName()) then
 		ChangeVanish(Player)
-	elseif Player:HasPermission("core.vanish.others") then
-		
+	elseif Player:HasPermission("es.vanish.others") then
 		if cRoot:Get():FindAndDoWithPlayer( Split[2], ChangeVanish ) then
 			Player:SendMessageSuccess( Split[2] ..  "s visibility has been toggled!")
 		else
-			SendMessageFailure( Player, "Player " .. Split[2] ..  " isn't online!")
+			SendMessageFailure("Player not found")
 		end
-		
 	else 
-		Player:SendMessageFailure("You need core.vanish.others permission to do that!")
+		Player:SendMessageFailure("You need es.vanish.others permission to do that!")
 	end
-	
 	return true
-	
 end
