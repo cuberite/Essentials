0f10eac84cd4e8133c7c86886dd15e109f854c99
ElNounch <publi@elnounch.net>
2016-08-11 22:33:29 +0000

    Warp: spaces in names, fix negative coordinates, listing filter and sort
    It takes a math.floor() to get correct integer from a negative coordinates
    Fixing portal signs look-up, were scanned up to a block away per X/Z axis
    Refresh warp list when reloading plugins
    Support for spaces in warp names
    Warp listing filter and sorting
    Proper signs detection (fix wall signs as portals)
    Splitting configuration data access from warp-stuff
    Moving teleport mechanism to specialized function
    Creating EverySecond()
    /back now also use recall world
    /back record position on death and server entrance
    Clean-up extra globals, typos
    Rotating player when warping to keep facing

--- warps.lua
+++ warps.lua
@@ -1,101 +1,181 @@
+local deltaPosition = Vector3d( 0.5, 0, 0.5 )
+
+local function GetListWarpByName()
+	local Result = {}
+	for k, _ in pairs (warps) do
+		table.insert( Result, k )
+	end
+	return Result
+end
+
+local function GetWarpByName( WarpName )
+	return warps[WarpName]
+end
+
+local function SetWarpByName( WarpName, NeoValue )
+	if (WarpsINI:FindKey(WarpName) == cIniFile.noID) then
+		WarpsINI:AddKeyName(WarpName)
+		WarpsINI:SetValue( WarpName , "w" , NeoValue.w )
+		WarpsINI:SetValueI( WarpName , "x" , NeoValue.position.x )
+		WarpsINI:SetValueI( WarpName , "y" , NeoValue.position.y )
+		WarpsINI:SetValueI( WarpName , "z" , NeoValue.position.z )
+		WarpsINI:SetValueI( WarpName , "f" , NeoValue.facing )
+		WarpsINI:WriteFile("warps.ini")
+		warps[WarpName] = NeoValue
+		return true
+	end
+	return false
+end
+
+local function UnsetWarpByName( WarpName )
+	if (WarpsINI:FindKey(WarpName) ~= cIniFile.noID) then
+		WarpsINI:DeleteKey(WarpName);
+		WarpsINI:WriteFile("warps.ini");
+		warps[WarpName] = nil
+		return true
+	end
+	return false
+end
+
+function LoadChunkAndTeleport( Player, Destination, PostActions )
+
+	PostActions = PostActions or {}
+	local OldWorld = Player:GetWorld()
+	local DestWorld
+	if ( Destination.w ) then
+		DestWorld = cRoot:Get():GetWorld(Destination.w)
+	else
+		DestWorld = OldWorld
+	end
+	local FinalPosition = Destination.position + deltaPosition
+
+	if Destination.facing then
+		table.insert(PostActions, function(Player)
+			Player:SetYaw((((Player:GetYaw() + 180 + 45) % 90 + Destination.facing * 90) % 360) - 45 - 180)
+		end)
+	end
+
+	if DestWorld then
+		local PlayerUID = Player:GetUniqueID()
+		local OnAllChunksAvailable = function()
+			OldWorld:DoWithEntityByID(PlayerUID, function (Entity)
+				local a_Player = tolua.cast(Entity, "cPlayer")
+				AwaitingPostWarpActions[PlayerUID] = PostActions
+				if ( DestWorld ~= OldWorld ) then
+					if ( change_gm_when_changing_world == true ) then
+						table.insert(PostActions, function(Player)
+							Player:SetGameMode(DestWorld:GetGameMode())
+						end)
+					end
+					a_Player:ScheduleMoveToWorld( DestWorld, FinalPosition )
+				else
+					a_Player:TeleportToCoords( FinalPosition.x, FinalPosition.y, FinalPosition.z )
+				end
+			end)
+		end -- OnAllChunksAvailable
+		DestWorld:ChunkStay({{ math.floor(FinalPosition.x / 16), math.floor(FinalPosition.z / 16) }}, nil, OnAllChunksAvailable)
+	end
+end
+
+local function ExtractWarpName( Parts )
+	local Tag = table.concat( Parts, ' ', 2 )
+	local Silent = false
+	if( Tag:sub(1,1) == '@' ) then
+		Tag = Tag:sub(2)
+		Silent = true
+	end
+
+	return Tag, Silent
+end
+
 function HandleWarpCommand( Split, Player )
-	if #Split < 2 then
-		--No warp given, list warps available.
+	if (#Split < 2) then
+		-- No warp given, list warps available.
 		HandleListWarpCommand( Split, Player )
 		return true
 	end
-	local Tag = Split[2]
-	
-	if warps[Tag] == nil then 
-		Player:SendMessageFailure('Warp "' .. Tag .. '" is invalid.')
+	local Tag, Silent = ExtractWarpName( Split )
+
+	local Destination = GetWarpByName( Tag )
+	if not(Destination) then
+		BackIgnoreNextTP[Player:GetUniqueID()] = nil	-- Set if we were on a misconfigured portal sign
+		Player:SendMessageFailure( string.format("Warp %s%s%s doesn't exist, yet.", cChatColor.Gold, Tag, cChatColor.Plain) )
 		return true
 	end
-	local OnAllChunksAvaliable = function()
-		if (Player:GetWorld():GetName() ~= warps[Tag]["w"]) then
-			Player:MoveToWorld(warps[Tag]["w"])
-			Player:TeleportToCoords( warps[Tag]["x"] + 0.5 , warps[Tag]["y"] , warps[Tag]["z"] + 0.5)
-			Player:SendMessageSuccess('Warped to "' .. Tag .. '".')
-			if change_gm_when_changing_world == true then
-				Player:SetGameMode(Player:GetWorld():GetGameMode())
-				return true
-			end
-		else
-			Player:TeleportToCoords( warps[Tag]["x"] + 0.5 , warps[Tag]["y"] , warps[Tag]["z"] + 0.5)
-			Player:SendMessageSuccess('Warped to "' .. Tag .. '".')
-		end
+
+	local PostActions = {}
+	if not(Silent) then
+		table.insert(PostActions, function(Player)
+			Player:SendMessage( string.format("Warped to %s%s%s.", cChatColor.LightGreen, Tag, cChatColor.Plain) )
+		end)
 	end
-	cRoot:Get():GetWorld(warps[Tag]["w"]):ChunkStay({{warps[Tag]["x"]/16, warps[Tag]["z"]/16}}, OnChunkAvailable, OnAllChunksAvaliable)
+
+	LoadChunkAndTeleport(Player, Destination, PostActions)
+
 	return true
 end
 
-function HandleSetWarpCommand( Split, Player)
-	local Server = cRoot:Get():GetServer()
-	local World = Player:GetWorld():GetName()
-	local pX = math.floor(Player:GetPosX())
-	local pY = math.floor(Player:GetPosY())
-	local pZ = math.floor(Player:GetPosZ())
-
+function HandleSetWarpCommand( Split, Player )
 	if #Split < 2 then
-		Player:SendMessageInfo('Usage: '..Split[1]..' <warpname>')
+		Player:SendMessageInfo( string.format("Usage: %s <warpname>", Split[1]) )
 		return true
 	end
-	local Tag = Split[2]
-	
-	if warps[Tag] == nil then 
-		warps[Tag] = {}
-	end
-	
-	if (WarpsINI:FindKey(Tag)<0) then
-		warps[Tag]["w"] = World
-		warps[Tag]["x"] = pX
-		warps[Tag]["y"] = pY
-		warps[Tag]["z"] = pZ
-	end
+	local Tag = ExtractWarpName( Split )
 
-	if (WarpsINI:FindKey(Tag)<0) then
-		WarpsINI:AddKeyName(Tag);
-		WarpsINI:SetValue( Tag , "w" , World)
-		WarpsINI:SetValue( Tag , "x" , pX)
-		WarpsINI:SetValue( Tag , "y" , pY)
-		WarpsINI:SetValue( Tag , "z" , pZ)
-		WarpsINI:WriteFile("warps.ini");
-	
-		Player:SendMessageSuccess("Warp \"" .. Tag .. "\" set to World:'" .. World .. "' x:'" .. pX .. "' y:'" .. pY .. "' z:'" .. pZ .. "'")
+	local NewWarp = {
+		w = Player:GetWorld():GetName(),
+		position = Vector3d(Player:GetPosition():Floor()),
+		facing = GetFacing(Player)
+	}
+
+	if SetWarpByName(Tag, NewWarp) then
+		local Destination = GetWarpByName( Tag )
+		if not(Destination) then
+			Player:SendMessageFatal( string.format("Warp %s%s%s not effectively set !", cChatColor.Gold, Tag, cChatColor.Plain) )
+			return true
+		end
+		Player:SendMessageSuccess( string.format( "Warp %s%s%s set to World: %s [X:%i Y:%i Z:%i]", cChatColor.LightGreen, Tag, cChatColor.Plain, Destination.w, Destination.position.x, Destination.position.y, Destination.position.z) )
 	else
-		Player:SendMessageFailure('Warp "' .. Tag .. '" already exists')
+		Player:SendMessageFailure( string.format( "Warp %s%s%s already exists or couldn't be set.", cChatColor.LightGreen, Tag, cChatColor.Plain) )
 	end
 	return true
 end
 
 function HandleDelWarpCommand( Split, Player)
-	local Server = cRoot:Get():GetServer()
-	
 	if #Split < 2 then
-		Player:SendMessageInfo('Usage: '..Split[1]..' <warp>')
+		Player:SendMessageInfo( string.format("Usage: %s <warp>", Split[1]) )
 		return true
 	end
-	local Tag = Split[2]
-	warps[Tag] = nil
+	local Tag = ExtractWarpName( Split )
 	
-	if (WarpsINI:FindKey(Tag)>-1) then
-		WarpsINI:DeleteKey(Tag);
-		WarpsINI:WriteFile("warps.ini");
+	if UnsetWarpByName(Tag) then
+		Player:SendMessageSuccess( string.format("Warp %s%s%s was removed.", cChatColor.LightGreen, Tag, cChatColor.Plain) )
 	else
-		Player:SendMessageFailure("Warp \"" .. Tag .. "\" was not found.")
-		return true
+		Player:SendMessageFailure( string.format("Warp %s%s%s doesn't exist or couldn't be removed.", cChatColor.LightGreen, Tag, cChatColor.Plain) )
 	end
 	
-	Player:SendMessageSuccess("Warp \"" .. Tag .. "\" was removed.")
 	return true
 end
 
 function HandleListWarpCommand( Split, Player)
-	local warpStr = ""
-	local inc = 0
-	for k, v in pairs (warps) do
-		inc = inc + 1
-		warpStr = warpStr .. k .. ", "
+	local Names = GetListWarpByName()
+	local FilteredBy = ""
+	if (#Split > 1) then
+		local Filter = string.lower(table.concat( Split, ' ', 2 ))
+		local Filtered = {}
+		for _, Name in pairs(Names) do
+			if string.find(string.lower(Name), Filter) then
+				table.insert(Filtered, Name)
+			end
+		end
+		if (#Filtered == 0) then
+			Player:SendMessage( string.format("No warp having a name matching '%s%s%s', try %s to see all warps", cChatColor.LightBlue, Filter, cChatColor.Plain, Split[1] ) )
+			return true
+		end
+		Names = Filtered
+		FilteredBy = "filtered by '" .. cChatColor.LightBlue .. Filter .. cChatColor.Plain .. "' "
 	end
-	Player:SendMessageInfo('Warps: ' ..  cChatColor.LightGreen ..  warpStr)
+	table.sort( Names )
+	Player:SendMessage( string.format("Warps %s: %s%s", FilteredBy, cChatColor.LightGreen, table.concat( Names, cChatColor.Plain .. ", " .. cChatColor.LightGreen ) ) )
 	return true
 end
